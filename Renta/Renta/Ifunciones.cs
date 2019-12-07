0dea58ef9ffed8ad7b64d98954521c612e4bf1ed
nerym <58615682+nerym@users.noreply.github.com>
2019-12-06 21:15:02 -0600

    Add files via upload

---
+++ Renta/Renta/Ifunciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Renta
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "Ifunciones" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface Ifunciones
+    {
+        [OperationContract]
+        void guardar(int a, string b, string c, string d, string e, string f);
+        [OperationContract]
+        bool modificar(int a, string b, string c, string d, string e, string f);
+        [OperationContract]
+        bool eliminar(int a);
+        [OperationContract]
+        string[] buscar(int cla);
+    }
+}
---
+++ Renta/Renta/funciones.svc.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Renta
+{
+    public class funciones : Ifunciones
+    {
+      public void guardar(int a, string b, string c, string d, string e, string f)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            string cadena = "";
+            con = new SqlConnection("Data Source=DESKTOP-CABRK5M; Initial Catalog=Rentas;Integrated Security=false;user=nery;password=12345;");
+            con.Open();
+            cadena = "insert into vestidoss values('" + a + "','" + b + "','" + c + "','" + d + "','" + e + "', '" + f + "')";
+            cmd = new SqlCommand(cadena, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public bool modificar(int a, string b, string c, string d, string e, string f)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            string cadena = "";
+            con = new SqlConnection("Data Source=DESKTOP-CABRK5M; Initial Catalog=Rentas;Integrated Security=false;user=nery;password=12345;");
+            con.Open();
+            cadena = "UPDATE vestidoss SET IDrenta= ('" + a + "'), Responsable = ('" + b + "'), Fecha_prestamo = ('" + c + "'), Fecha_entrega = ('" + d + "'), Descripcion = ('" + e + "'), Monto = ('" + f + "')where IDrenta = ('" + a + "')";
+            cmd = new SqlCommand(cadena, con);
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            con.Close();
+
+        }
+        public bool eliminar(int a)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            string cadena = "";
+            con = new SqlConnection("Data Source=DESKTOP-CABRK5M; Initial Catalog=Rentas;Integrated Security=false;user=nery;password=12345;");
+            con.Open();
+            cadena = "delete from vestidoss where IDrenta=" + a;
+            cmd = new SqlCommand(cadena, con);
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+            con.Close();
+        }
+        public string[] buscar(int cla)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            SqlDataReader dato;
+            string cadena = "";
+            string[] datos = new string[6];
+            con = new SqlConnection("Data Source=DESKTOP-CABRK5M; Initial Catalog=Rentas;Integrated Security=false;user=nery;password=12345;");
+            con.Open();
+            cadena = "select * from vestidoss where IDrenta=" + cla ;
+            cmd = new SqlCommand(cadena, con);
+            dato = cmd.ExecuteReader();
+            if (dato.Read())
+            {
+                datos[0] = dato.GetInt64(0).ToString();
+                datos[1] = dato.GetString(1);
+                datos[2] = dato.GetString(2);
+                datos[3] = dato.GetString(3);
+                datos[4] = dato.GetString(4);
+                datos[5] = dato.GetString(5);
+            }
+            con.Close();
+            return datos;
+        }
+    }
+}
